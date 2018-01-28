1cc3e02a4bd89c0cff095bac5615d7b31af393c4
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-01-27 23:17:35 -0800

    Cleaning things out, relatively unimportant

---
+++ Assets/AudioTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour {
+
+    public float audioFadeAmount;
+    public GameObject audioThing;
+    public GameObject audioThingList;
+    private bool isActive;
+    private AudioSource audSou;
+
+    void Start () {
+        isActive = audioThing && audioThing.GetComponent<AudioSource>();
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isActive)
+        {
+            GameObject temp = Instantiate(audioThing, audioThingList.transform);
+            audSou = temp.GetComponent<AudioSource>();
+            audSou.volume = 0;
+            StartCoroutine("fadeIn");
+        }
+    }
+    IEnumerator fadeIn()
+    {
+        do {
+            audSou.volume += audioFadeAmount;
+            yield return null;
+        } while (audSou.volume + audioFadeAmount < 1);
+        audSou.volume = 1;
+    }
+}
