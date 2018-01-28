e0b420898a614e9daf00614b488730a81d9e5607
EPKgit <ericpaulkim@gmail.com>
2018-01-28 11:28:22 -0800

    More menu stuff

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -5,10 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour 
 {
+	public static MenuManager instance;
+
 	public string sceneName;
+
+	public void Awake()
+	{
+		if(instance != null)
+			Destroy(this.gameObject);
+		instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
 
 	public void StartGame()
 	{
 		SceneManager.LoadScene(sceneName);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public void OpenSettingsMenu()
+	{
+
 	}
 }
