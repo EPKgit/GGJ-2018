5efc69183a1330ddcab9588c6d31647c6c1146f0
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-01-28 14:42:38 -0800

    Add Precursor Audio Volume Manager Script

---
+++ Assets/changeVol.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeVol : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        this.gameObject.transform.Find("01_01").GetComponent<AudioSource>().volume = MenuManager.instance.volume;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
