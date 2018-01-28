de324ec6053749e884eca675ab48593d0937c176
Daniel Schauer <33043339+SiGTecan@users.noreply.github.com>
2018-01-28 11:50:32 -0800

    Dynamic music added
    DanielTest.Unity now has dynamically activating layered music that fades in as you progress

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour {
+
+    //public bool modfirstAudSource;
+    public float audioFadeAmount;
+    public float maxVolume;
+    public AudioClip givenClip;
+    public GameObject audioThing;
+    //public GameObject audioThingList;
+    private bool isActive, hasAlreadyActivated = false;
+    private AudioSource audSou;
+
+    void Start () {
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Collision Detected!");
+        if(!hasAlreadyActivated && other.gameObject.tag == "Player")
+        {
+            GameObject temp = Instantiate(audioThing, other.transform);
+            //audSou = modfirstAudSource ? other.gameObject.GetComponent<AudioSource>() : other.transform.Find("AudioSource").GetComponent<AudioSource>();
+            audSou = temp.GetComponent<AudioSource>();
+            audSou.clip = givenClip;
+            audSou.Play();
+            //audSou.outputAudioMixerGroup
+            audSou.volume = 0;
+            audSou.loop = true;
+            StartCoroutine("FadeIn");
+            hasAlreadyActivated = true;
+        }
+    }
+    IEnumerator FadeIn()
+    {
+        do {
+            audSou.volume += audioFadeAmount;
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+            //only increments once every 5 ticks b/c its far too fast otherwise
+        } while (audSou.volume + audioFadeAmount < maxVolume);
+        audSou.volume = maxVolume;
+    }
+}
---
+++ Assets/Scripts/fadeBox.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeBox : MonoBehaviour {
+
+    //public bool modfirstAudSource;
+    public float audioFadeAmount;
+    public float newVolume;
+    public string trackLayerName;
+    private AudioSource audSou;
+    private bool hasAlreadyActivated = false;
+
+    void Start()
+    {
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Collision Detected!");
+        if (!hasAlreadyActivated && other.gameObject.tag == "Player")
+        {
+            audSou = other.transform.Find(trackLayerName).GetComponent<AudioSource>();
+            StartCoroutine("FadeIn");
+            hasAlreadyActivated = true;
+        }
+    }
+    IEnumerator FadeIn()
+    {
+        do
+        {
+            audSou.volume += audioFadeAmount;
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+            yield return null;
+            //only increments once every 5 ticks b/c its far too fast otherwise
+        } while (audSou.volume + audioFadeAmount < newVolume);
+        audSou.volume = newVolume;
+    }
+}
